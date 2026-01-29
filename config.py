fb998be01fc45dab60a34230834e845f7cbf7c43
KARTIKSAINI18 <152260259+KARTIKSAINI18@users.noreply.github.com>
2026-01-29 19:38:21 +0530

    Add DYNAMODB_TABLE_ALERTS configuration

--- config.py
+++ config.py
@@ -4,6 +4,7 @@ class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
 
     AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
+    DYNAMODB_TABLE_ALERTS = os.environ.get("DYNAMODB_TABLE_ALERTS", "Alerts")
 
     DYNAMODB_TABLE_USERS = os.environ.get("DYNAMODB_TABLE_USERS", "Users")
     
