35070e64476aea39e45505f33337b6fc44649b25
KARTIKSAINI18 <152260259+KARTIKSAINI18@users.noreply.github.com>
2026-01-29 19:44:04 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,8 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get("SECRET_KEY", "dev-secret-key")
+    SNS_TOPIC_ARN = os.environ.get("SNS_TOPIC_ARN")
+
 
     AWS_REGION = os.environ.get("AWS_REGION", "us-east-1")
     DYNAMODB_TABLE_ALERTS = os.environ.get("DYNAMODB_TABLE_ALERTS", "Alerts")
